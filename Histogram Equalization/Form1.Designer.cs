b88b33af40cca64c6bc75fbbb0af6242e797393f
parkmftsai <parktasi@gmail.com>
2015-10-12 10:02:31 +0800

    parktasi@gmail.com

---
+++ Histogram Equalization/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Histogram_Equalization
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.Browse = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(155, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Browse
+            // 
+            this.Browse.Location = new System.Drawing.Point(12, 39);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(75, 23);
+            this.Browse.TabIndex = 1;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.button1_Browse);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Trancefer";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_trancefer);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(601, 39);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click_1);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(601, 320);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(155, 320);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 251);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 592);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Histogram Equalization/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Histogram_Equalization
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Bitmap show_picture;
+            button1.Enabled = false;
+            button2.Enabled = false;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Bitmap show_picture;
+
+        }
+
+        private void button1_Browse(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            button2.Enabled = false;
+
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp; *.png; *.tif)|*.jpg; *.jpeg; *.gif; *.bmp; *.png; *.tif";
+
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                /*相關參數設定*/
+                Bitmap tempbitmap = new Bitmap(open.FileName);
+                Bitmap input = new Bitmap(tempbitmap.Width / 2, tempbitmap.Height / 2);
+                Bitmap output = new Bitmap(250, 250);
+
+                int max = 0;
+                int[] number = new int[256];
+
+                double[] p = new double[256];
+
+                int i, j, x, y;
+                //縮小
+                int w = (int)(tempbitmap.Width / 2);
+                int h = (int)(tempbitmap.Height / 2);
+                if (tempbitmap.Height > 256 && tempbitmap.Width > 256)
+                {
+                    for (j = 0; j < tempbitmap.Height / 2; j++)
+                        for (i = 0; i < tempbitmap.Width / 2; i++)
+                        {
+
+                            y = (int)(j * 2 + 0.5); x = (int)(i * 2 + 0.5); //四捨五入
+
+                            if (x >= tempbitmap.Width)
+                                x = tempbitmap.Width + (tempbitmap.Width - x - 1);
+                            if (y >= tempbitmap.Height)
+                                y = tempbitmap.Height + (tempbitmap.Height - y - 1);
+                            Color getpixel = tempbitmap.GetPixel(x, y);
+                            if (i >= w)
+                                i = w + (w - i - 1);
+                            if (j >= h)
+                                j = h + (h - j - 1);
+                            input.SetPixel(i, j, Color.FromArgb(getpixel.R, getpixel.G, getpixel.B));
+
+                        }
+                    pictureBox1.Image = input;
+                }
+                else
+                {
+                    pictureBox1.Image = tempbitmap;
+                }
+
+                /*繪製input直方圖*/
+                for (i = 0; i < 256; i++)
+                    number[i] = 0;
+
+                for (j = 0; j < tempbitmap.Height; j++)
+                    for (i = 0; i < tempbitmap.Width; i++)
+                    {
+                        Color getanpixel = tempbitmap.GetPixel(i, j);
+                        number[getanpixel.R]++;
+                        if (number[getanpixel.R] > max)
+                            max = number[getanpixel.R];
+                    }
+
+
+                Graphics g2 = Graphics.FromImage((Image)output);
+                pictureBox2.BackColor = Color.Black;
+                Pen myPen = new Pen(new SolidBrush(Color.White), 1);
+                int x1, y1, x2, y2;
+                double ratey = (pictureBox2.Height * 1.0) / (max * 1.0);
+                double ratex = (pictureBox2.Width * 1.0) / (255 * 1.0);
+                int y_height;
+
+                for (i = 0; i <= pictureBox2.Width; i++)
+                {
+                    x1 = i;
+                    x2 = x1;
+                    y1 = pictureBox2.Height;
+                    y_height = Convert.ToInt32(number[Convert.ToInt32(i / ratex)] * ratey);
+                    y2 = pictureBox2.Height - y_height;
+                    g2.DrawLine(myPen, x1, y1, x2, y2);
+
+                }
+                pictureBox2.Image = (Image)output;
+                pictureBox2.Refresh();
+                button1.Enabled = true;
+                Browse.Enabled = false;
+
+            }
+        }
+
+        private void button1_trancefer(object sender, EventArgs e)
+        {
+            /*相關參數設定*/
+            Bitmap an = (Bitmap)pictureBox1.Image;
+            Bitmap input = new Bitmap(250, 250);
+            Bitmap output = new Bitmap(250, 250);
+            Bitmap his = new Bitmap(an.Width, an.Height);
+            int i, j, x, y, th, k, max = 0;
+            int[] number = new int[256];
+            double[] p = new double[256];
+            Graphics g2 = Graphics.FromImage((Image)input);
+            Pen myPen = new Pen(new SolidBrush(Color.White), 1);
+            int x1, y1, x2, y2;
+            double ratey = (pictureBox2.Height * 1.0) / (max * 1.0);
+            double ratex = (pictureBox2.Width * 1.0) / (255 * 1.0);
+            int y_height;
+
+            button1.Enabled = false;
+
+            /*以下程式使用Histogram Equalization Algorithm*/
+            for (i = 0; i < 256; i++)
+                p[i] = number[i] = 0;
+
+            //統計
+            for (j = 0; j < an.Height; j++)
+                for (i = 0; i < an.Width; i++)
+                {
+                    Color getanpixel = an.GetPixel(i, j);
+                    number[getanpixel.R]++;
+
+                    if (number[getanpixel.R] > max)
+                        max = number[getanpixel.R];
+                }
+
+            //算機率(平均值means)
+            for (i = 0; i < 256; i++)
+            {
+                p[i] = (double)number[i] / (double)(an.Height * an.Width);
+            }
+            //累進機率(每個人都會需要彼此)    
+            for (i = 1; i < 256; i++)
+                p[i] = p[i] + p[i - 1];
+
+
+            //重新取代   
+            for (i = 0; i < 256; i++)
+            {
+                number[i] = (int)((double)(p[i]) * 255 + 0.5);
+            }
+
+
+            //根據對應的顏色，用新的顏色取代
+            for (j = 0; j < an.Height; j++)
+                for (i = 0; i < an.Width; i++)
+                {
+                    Color getanpixel = an.GetPixel(i, j);
+                    int n = getanpixel.R;
+
+                    his.SetPixel(i, j, Color.FromArgb(number[n], number[n], number[n]));
+                }
+            //接圖
+            pictureBox4.Image = his;
+
+
+            /*繪製output直方圖*/
+            max = 0;
+            for (i = 0; i < 256; i++)
+                number[i] = 0;
+            for (j = 0; j < his.Height; j++)
+                for (i = 0; i < his.Width; i++)
+                {
+                    Color getanpixel = his.GetPixel(i, j);
+                    number[getanpixel.R]++;
+                    if (number[getanpixel.R] > max)
+                        max = number[getanpixel.R];
+                }
+
+            g2 = Graphics.FromImage((Image)output);
+            pictureBox3.BackColor = Color.Black;
+            ratey = (pictureBox3.Height * 1.0) / (max * 1.0);
+            ratex = (pictureBox3.Width * 1.0) / (255 * 1.0);
+
+            for (i = 0; i <= pictureBox3.Width; i++)
+            {
+                x1 = i;
+                x2 = x1;
+                y1 = pictureBox3.Height;
+                y_height = Convert.ToInt32(number[Convert.ToInt32(i / ratex)] * ratey);
+                y2 = pictureBox3.Height - y_height;
+                g2.DrawLine(myPen, x1, y1, x2, y2);
+            }
+
+            pictureBox3.Image = (Image)output;
+            pictureBox3.Refresh();
+            button2.Enabled = true;
+        }
+
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox4.BackColor = Color.Transparent;
+            pictureBox2.Image = null;
+            pictureBox3.Image = null;
+            pictureBox1.Image = null;
+            pictureBox4.Image = null;
+            button2.Enabled = false;
+            Browse.Enabled = true;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
